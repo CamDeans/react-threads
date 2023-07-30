b42767f383c330231708b67dd1fea24c5027485b
Cameron Deans <cameron.deans@student.sl.on.ca>
2023-07-30 10:16:50 -0400

    Sixth Commit - Users functionality completed

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Nav from "./components/Nav"
 
 const App = () => {
   const [ user, setUser ] = useState(null)
+  const [ threads, setThreads ] = useState(null)
+  const [ viewThreadsFeed, setViewThreadsFeed ] = useState(true)
 
   const userId = "91f16eb3-b041-47f0-b538-267c5b77cd12"
 
   const getUser = async () => {
     try {
-      const response = await fetch(`localhost:3000/users?user_uuid=${userId}`)
+      const response = await fetch(`http://localhost:3000/users?user_uuid=${userId}`)
       const data = await response.json()
       setUser(data[0])
     } catch (error) {
@@ -22,20 +24,43 @@ const App = () => {
     }
   }
 
+  const getThreads = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/threads?thread_from${userId}`)
+      const data = await response.json()
+      setThreads(data[0])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getThreadsFeed = () => {
+
+  }
+
   useEffect(() => {
     getUser()
+    getThreads()
   }, [])
 
-  console.log(user)
+  console.log(user, threads)
 
   return (
-    <div className="app">
-     <Nav></Nav>
-     <Header></Header>
-     <Feed></Feed> 
-     {/*<PopUp></PopUp>*/}
-    </div>
+    <>
+      {
+        user && <div className="app">
+        <Nav url={user.instagram_url} />
+        <Header 
+          user={user}
+          viewThreadsFeed={viewThreadsFeed}  
+          setViewThreadsFeed={setViewThreadsFeed}
+        />
+        <Feed />
+        {/*<PopUp />*/}
+        </div>
+      }
+    </>
   );
 }
 
-export default App;
+export default App
